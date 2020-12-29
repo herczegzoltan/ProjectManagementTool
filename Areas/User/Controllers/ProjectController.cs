eff95e2d7b88fcb6aa553f53df42aef89854c1f2
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-29 19:44:15 +0100

    Adding Project controller and view

---
+++ Areas/User/Controllers/ProjectController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagementTool.Areas.User.Controllers
+{
+    [Area("User")]
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
