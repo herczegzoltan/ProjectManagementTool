30fe57bc334e208eb10aa067fe43d20f62819ecd
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-29 19:47:11 +0100

    Add DI to ProjectController

--- Areas/User/Controllers/ProjectController.cs
+++ Areas/User/Controllers/ProjectController.cs
@@ -3,15 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagementTool.Data;
 
 namespace ProjectManagementTool.Areas.User.Controllers
 {
     [Area("User")]
     public class ProjectController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+        
+        public ProjectController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Projects.ToListAsync());
         }
     }
 }
