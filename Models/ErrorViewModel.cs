8a69cbf821fd7ad4bae247e248f3df51055af3d8
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-29 17:57:47 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectManagementTool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
