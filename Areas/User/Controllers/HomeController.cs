36508bcf768d25edd59c7f6600279c2d93bfab0a
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-29 18:17:02 +0100

    Moving files to make areas working

--- Areas/User/Controllers/HomeController.cs
+++ Areas/User/Controllers/HomeController.cs
@@ -7,8 +7,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ProjectManagementTool.Controllers
+namespace ProjectManagementTool.Areas.User.Controllers
 {
+    [Area("User")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
