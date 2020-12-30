9f881aca77f53c698b59fb001f8a7828f241f38b
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-30 14:21:56 +0100

    Add extra properties to Task Model

--- Models/Task.cs
+++ Models/Task.cs
@@ -12,8 +12,37 @@ namespace ProjectManagementTool.Models
         public string Description { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
-        public string Status { get; set; }
+
         public string Type { get; set; }
+        public enum EType
+        {
+            Test = 0,
+            Task = 1,
+            Bug = 2,
+            NewFeature = 3,
+            Improvement = 4,
+        }
+
+        public string Status { get; set; }
+        public enum EStatus
+        {
+            New = 0,
+            Closed = 1,
+            InProgress = 2,
+            OnHold = 3,
+        }
+        
         public string Priority { get; set; }
+        public enum EPriority
+        {
+            Blocker = 0,
+            Major = 1,
+            High = 2,
+            Medium = 3,
+            Normal = 4,
+            Low = 5,
+            Minor = 6,
+            None = 7
+        }
     }
 }
