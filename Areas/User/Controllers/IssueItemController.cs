f95b89c1767f8e46699653d603656346edb7c79b
Zoltán Herczeg <herczzo1@corp.onespan.com>
2020-12-30 20:45:51 +0100

    Create IssueItem Post and recive already existing ones on Index

---
+++ Areas/User/Controllers/IssueItemController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagementTool.Data;
+using ProjectManagementTool.Models;
+using ProjectManagementTool.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+
+namespace ProjectManagementTool.Areas.User.Controllers
+{
+    [Area("User")]
+    public class IssueItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        ////[BindProperty]
+        public IssueItemViewModel IssueItemViewModel { get; set; }
+
+        public IssueItemController(ApplicationDbContext db)
+        {
+            _db = db;
+            IssueItemViewModel = new IssueItemViewModel();
+        }
+
+        // GET
+        public async Task<IActionResult> Index(int id)
+        {
+            // TODO in case id is null i need to validate
+            
+            var issueItemsByProductId = await _db.Projects.Where(p=>p.Id == id).SelectMany(i=>i.IssueItems).ToListAsync();
+
+            IssueItemViewModel.Project = await _db.Projects.FindAsync(id);
+            IssueItemViewModel.IssueItems = issueItemsByProductId;
+
+            HttpContext.Session.SetInt32("selectedProjectId", id);
+
+            return View(IssueItemViewModel);
+        }
+
+        // GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }  
+        
+        // POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(IssueItem issueItem)
+        {
+            if (ModelState.IsValid)
+            {
+                var selectedProjectId = HttpContext.Session.GetInt32("selectedProjectId");
+
+                if (selectedProjectId != null)
+                {
+                    var selectedProject = await _db.Projects.FindAsync(selectedProjectId);
+
+                    if (selectedProject.IssueItems == null)
+                    {
+                        selectedProject.IssueItems = new List<IssueItem>() {issueItem};
+                    }
+                    else
+                    {
+                        selectedProject.IssueItems.Add(issueItem);
+                    }
+                }
+
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index",new {id = selectedProjectId});
+            }
+
+            return View(issueItem);
+        }
+    }
+}
